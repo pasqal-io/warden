22ed50fbbde7ee97157de4ca9ea9dfc655484c2f
badtst <thomas.badts@pasqal.com>
2026-04-13 09:34:15 +0000

    Absolute imports

--- warden/lib/models/__init__.py
+++ warden/lib/models/__init__.py
@@ -1,7 +1,6 @@
 from warden.lib.db.database import Base
-
-from .accessible import AccessibilitySettings
-from .jobs import Job
-from .sessions import Session
+from warden.lib.models.accessible import AccessibilitySettings
+from warden.lib.models.jobs import Job
+from warden.lib.models.sessions import Session
 
 __all__ = ["Base", "Job", "Session", "AccessibilitySettings"]
