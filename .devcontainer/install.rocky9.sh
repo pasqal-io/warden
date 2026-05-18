cb82d60c07be14395b4955fc00636c1674dce422
Sam Groot <samuel.groot-ext@pasqal.com>
2026-05-18 15:17:04 +0200

    test(ci): Test install in different environments (#20)

---
+++ .devcontainer/install.rocky9.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+set -ex
+
+dnf makecache
+dnf -y update
+dnf -y install dnf-plugins-core
+dnf config-manager --set-enabled crb
+dnf -y install \
+  wget \
+  bzip2 \
+  perl \
+  gcc \
+  gcc-c++\
+  git \
+  gnupg \
+  make \
+  munge \
+  munge-devel \
+  python${PYTHON_VERSION}-devel \
+  python${PYTHON_VERSION}-pip \
+  python${PYTHON_VERSION} \
+  mariadb \
+  mariadb-server \
+  mariadb-devel \
+  postgresql \
+  psmisc \
+  bash-completion \
+  vim-enhanced \
+  http-parser-devel \
+  json-c-devel \
+  cmake \
+  clang-tools-extra \
+  procps \
+  iputils \
+  net-tools \
+  openblas-devel \
+  jq \
+  iputils \
+  net-tools \
+  libyaml-devel \
+  procps \
+  lua-devel
+dnf clean all
+rm -rf /var/cache/dnf
+
+alternatives --install /usr/bin/python3 python3 /usr/bin/python${PYTHON_VERSION} 1
--- .devcontainer/rocky8.Dockerfile
+++ .devcontainer/rocky8.Dockerfile
@@ -1,10 +1,10 @@
-FROM rockylinux:9
+FROM rockylinux:8
 
 ARG PYTHON_VERSION=3.11
 ENV PYTHON_VERSION=${PYTHON_VERSION}
 
-COPY .devcontainer/install.rocky9.sh /tmp/install.rocky9.sh
-RUN sh /tmp/install.rocky9.sh
+COPY .devcontainer/install.rocky8.sh /tmp/install.rocky8.sh
+RUN sh /tmp/install.rocky8.sh
 
 RUN groupadd --gid 1000 devuser && \
   useradd --uid 1000 --gid 1000 -m -s /bin/bash devuser
