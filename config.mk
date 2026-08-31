1b2ec70c7dfaf7f5a3ff31b51530659b1c929110
badtst <thomas.badts@pasqal.com>
2026-08-31 10:00:38 +0000

    minor

--- config.mk
+++ config.mk
@@ -2,8 +2,8 @@
 
 # Whether to install the PostgreSQL/MariaDB packages database respectively
 # If you do decide to change the actual backend, you will have to run the install command again
-WITH_PG?=0
-WITH_MARIADB?=0
+WITH_PG=0
+WITH_MARIADB=0
 
 # The Python interpreter to use, 3.11 and 3.12 are supported
 PYTHON?=python3
